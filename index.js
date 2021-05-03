4c0bafd576816006b0221e4713496f740863ef54
Admin <hudaierbas@gmail.com>
2021-05-03 18:42:03 +0300

    drag function added

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 var map = document.getElementById("map");
 var mapContainer = document.getElementById("map__container");
 
-window.addEventListener("resize", () => {
-  setMapContainerToMiddle();
-});
+var mapContainerX;
+var mapContainerY;
+var mouseX;
+var mouseY;
 
-//set map to center of screen
-const setMapContainerToMiddle = () => {
-  let mapWidth = map.clientWidth;
-  let mapHeight = map.clientHeight;
-  let mapContainerWidth = mapContainer.clientWidth;
-  let mapContainerHeight = mapContainer.clientHeight;
+// window.addEventListener("resize", () => {
+//   setMapContainerToMiddle();
+// });
 
-  let mapContainerTop = (mapHeight - mapContainerHeight) / 2;
-  let mapContainerLeft = (mapWidth - mapContainerWidth) / 2;
+// //set map to center of screen
+// const setMapContainerToMiddle = () => {
+//   let mapWidth = map.clientWidth;
+//   let mapHeight = map.clientHeight;
+//   let mapContainerWidth = mapContainer.clientWidth;
+//   let mapContainerHeight = mapContainer.clientHeight;
 
-  mapContainer.style.top = mapContainerTop + "px";
-  mapContainer.style.left = mapContainerLeft + "px";
-};
+//   let mapContainerTop = (mapHeight - mapContainerHeight) / 2;
+//   let mapContainerLeft = (mapWidth - mapContainerWidth) / 2;
 
-setMapContainerToMiddle();
+//   mapContainer.style.top = mapContainerTop + "px";
+//   mapContainer.style.left = mapContainerLeft + "px";
+// };
+
+// setMapContainerToMiddle();
 
 //change map size
 const mapSize = (e) => {
@@ -44,3 +49,64 @@ mapContainer.addEventListener("wheel", (event) => {
     mapSize("decrease");
   }
 });
+
+//drag the map
+function startDrag(e) {
+  // determine event object
+  if (!e) {
+    var e = window.event;
+    console.log(e);
+  }
+
+  // IE uses srcElement, others use target
+  var targ = e.target ? e.target : e.srcElement;
+
+  if (targ.className != "map__container") {
+    return;
+  }
+  // calculate event X, Y coordinates
+  offsetX = e.clientX;
+  offsetY = e.clientY;
+
+  // assign default values for top and left properties
+  if (!targ.style.left) {
+    targ.style.left = "0px";
+  }
+  if (!targ.style.top) {
+    targ.style.top = "0px";
+  }
+
+  // calculate integer values for top and left
+  // properties
+  coordX = parseInt(targ.style.left);
+  coordY = parseInt(targ.style.top);
+  drag = true;
+
+  // move div element
+  document.onmousemove = dragDiv;
+
+  return false;
+}
+
+function dragDiv(e) {
+  if (!drag) {
+    return;
+  }
+  if (!e) {
+    var e = window.event;
+  }
+  var targ = e.target ? e.target : e.srcElement;
+  // move div element
+  targ.style.left = coordX + e.clientX - offsetX + "px";
+  targ.style.top = coordY + e.clientY - offsetY + "px";
+  return false;
+}
+
+function stopDrag() {
+  drag = false;
+}
+
+window.onload = function () {
+  document.onmousedown = startDrag;
+  document.onmouseup = stopDrag;
+};
