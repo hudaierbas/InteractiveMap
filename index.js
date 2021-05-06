bb9a5a2874195c247d3f2d131465e1b71b2165f1
Admin <hudaierbas@gmail.com>
2021-05-07 00:35:40 +0300

    map pin added, map size fixed

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 var map = document.getElementById("map");
 var mapContainer = document.getElementById("map__container");
+var scene = document.getElementById("scene");
 
 //change map size
-const mapSize = (e) => {
+var increaseRate = mapContainer.clientWidth / mapContainer.clientHeight;
+
+const mapSize = (x) => {
+  //map current size
   let mapContainerWidth = mapContainer.clientWidth;
   let mapContainerHeight = mapContainer.clientHeight;
 
-  if (e === "increase") {
-    mapContainer.style.width = mapContainerWidth + 100 + "px";
-    mapContainer.style.height = mapContainerHeight + 100 + "px";
+  if (x === "increase") {
+    //increase map size
+    mapContainer.style.width = mapContainerWidth + 100 * increaseRate + "px";
+    mapContainer.style.height = mapContainerHeight + 100 * increaseRate + "px";
   }
-  if (e === "decrease") {
-    mapContainer.style.width = mapContainerWidth - 100 + "px";
-    mapContainer.style.height = mapContainerHeight - 100 + "px";
+  if (x === "decrease") {
+    //decrease map size
+    mapContainer.style.width = mapContainerWidth - 100 * increaseRate + "px";
+    mapContainer.style.height = mapContainerHeight - 100 * increaseRate + "px";
   }
-  setMapContainerToMiddle();
 };
 
-mapContainer.onwheel = (event) => {
+scene.onwheel = (event) => {
   if (event.deltaY < 0) {
+    //on mouse wheel up
     mapSize("increase");
   } else if (event.deltaY > 0) {
+    //on mouse wheel down
     mapSize("decrease");
   }
 };
@@ -36,7 +43,7 @@ const startDrag = (e) => {
   // IE uses srcElement, others use target
   var targ = e.target ? e.target : e.srcElement;
 
-  if (targ.className != "map__container") {
+  if (targ.className != "dragme") {
     return;
   }
   // calculate event X, Y coordinates
@@ -63,6 +70,7 @@ const startDrag = (e) => {
   return false;
 };
 
+//move map
 const dragDiv = (e) => {
   if (!drag) {
     return;
@@ -71,7 +79,7 @@ const dragDiv = (e) => {
     var e = window.event;
   }
   var targ = e.target ? e.target : e.srcElement;
-  // move div element
+  // move map
   targ.style.left = coordX + e.clientX - offsetX + "px";
   targ.style.top = coordY + e.clientY - offsetY + "px";
   return false;
@@ -85,3 +93,50 @@ window.onload = () => {
   document.onmousedown = startDrag;
   document.onmouseup = stopDrag;
 };
+
+//test data
+var testData = {
+  1: {
+    title: "data1",
+    info: "info 1 info 1info 1info 1info 1info 1info 1info 1info 1",
+    coordX: "310",
+    coordY: "210",
+  },
+  2: {
+    title: "data2",
+    info: "info 2 info 2info 2info 2info 2info 2info 2info 2info 2",
+    coordX: "200",
+    coordY: "300",
+  },
+  3: {
+    title: "data2",
+    info: "info 2 info 2info 2info 2info 2info 2info 2info 2info 2",
+    coordX: "200",
+    coordY: "400",
+  },
+};
+
+var count = Object.keys(testData).length;
+
+//on hover pin
+const pinHover = (pinNo) => {
+  console.log(pinNo);
+};
+
+//create pins
+const createPin = () => {
+  for (let index = 1; index < count + 1; index++) {
+    const element = testData[`${index}`];
+
+    const pin = document.createElement("img");
+    pin.className = "pin";
+    pin.src = "./img/pin.png";
+    pin.setAttribute("onmouseover", `pinHover(${index})`);
+    pin.style.top = (element.coordY / mapContainer.clientHeight) * 100 + "%";
+    pin.style.left = (element.coordX / mapContainer.clientWidth) * 100 + "%";
+
+    scene.appendChild(pin);
+  }
+};
+
+createPin();
